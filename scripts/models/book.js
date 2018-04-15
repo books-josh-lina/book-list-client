80cb06480db162923f703c9675c2af265a1e1d40
Laboni A <asthanal@yahoo.com>
2018-04-14 17:54:11 -0700

    Finished the book-list-client page

---
+++ scripts/models/book.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var app = app || {};
+const ENV = {};
+
+// ENV.isProduction = window.location.protocol === 'https:';
+ENV.productionApiUrl = 'https://books-josh-lina.github.io/book-list-client';
+ENV.developmentApiUrl = 'http://localhost:3000';
+ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
+
+(function(module) {
+  function Book(rawDataObj) {
+    // REVIEW: In Lab 8, we explored a lot of new functionality going on here. Let's re-examine the concept of context. Normally, "this" inside of a constructor function refers to the newly instantiated object. However, in the function we're passing to forEach, "this" would normally refer to "undefined" in strict mode. As a result, we had to pass a second argument to forEach to make sure our "this" was still referring to our instantiated object. One of the primary purposes of lexical arrow functions, besides cleaning up syntax to use fewer lines of code, is to also preserve context. That means that when you declare a function using lexical arrows, "this" inside the function will still be the same "this" as it was outside the function. As a result, we no longer have to pass in the optional "this" argument to forEach!
+    Object.keys(rawDataObj).forEach(key => this[key] = rawDataObj[key]);
+  }
+
+
+  Book.prototype.toHtml = function() {
+    var template = Handlebars.compile($('#book-list-template').text());
+    return template(this);
+  };
+
+  Book.all = [];
+
+  Book.loadAll = rows => {
+    Book.all = rows.sort((a,b) => b.title - a.title).map(book => new Book(book));
+  }
+
+  Book.fetchAll = callback => {
+    $.get(`${ENV.apiUrl}/api/v1/books`)
+      .then(Book.loadAll)
+      .then(callback)
+      .catch(app.errorCallback)  
+  }
+  module.Book = Book;
+})(app)
+
+
---
+++ scripts/views/book-view.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var app = app || {};
+
+(function(module){
+  const bookView = {};
+
+  bookView.initIndexPage = function() {
+    $('.container').hide();
+    $('.book-view').show();
+    console.log('init index page');
+    app.Book.all.map(book => $('#book-list').append(book.toHtml()));
+  }
+  module.bookView = bookView;
+})(app)
+
+$(function() {
+  console.log('in IIFE');
+  app.Book.fetchAll(app.bookView.initIndexPage);
+})
+
