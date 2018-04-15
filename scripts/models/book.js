a7179bc1da2b3248ffa7b18d5e64543a1a77a445
Laboni A <asthanal@yahoo.com>
2018-04-14 18:02:39 -0700

    added changes to book.js

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -3,8 +3,8 @@
 var app = app || {};
 const ENV = {};
 
-// ENV.isProduction = window.location.protocol === 'https:';
-ENV.productionApiUrl = 'https://books-josh-lina.github.io/book-list-client';
+ENV.isProduction = window.location.protocol === 'https:';
+ENV.productionApiUrl = 'https://jpm-lm-tc-booklist.herokuapp.com/';
 ENV.developmentApiUrl = 'http://localhost:3000';
 ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
 
