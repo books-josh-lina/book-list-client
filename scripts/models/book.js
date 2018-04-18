1cde5fbb7f280ec813908443c3f68dd9375615c1
Jozue06 <jozue06@gmail.com>
2018-04-17 22:43:57 -0700

    small updates

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -7,18 +7,15 @@ ENV.isProduction = window.location.protocol === 'https:';
 ENV.productionApiUrl = 'https://jpm-lm-tc-booklist.herokuapp.com';
 ENV.developmentApiUrl = 'http://localhost:3000';
 ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
-// console.log('is ' + ENV);
 
 (function(module) {
   function Book(rawDataObj) {
-    // REVIEW: In Lab 8, we explored a lot of new functionality going on here. Let's re-examine the concept of context. Normally, "this" inside of a constructor function refers to the newly instantiated object. However, in the function we're passing to forEach, "this" would normally refer to "undefined" in strict mode. As a result, we had to pass a second argument to forEach to make sure our "this" was still referring to our instantiated object. One of the primary purposes of lexical arrow functions, besides cleaning up syntax to use fewer lines of code, is to also preserve context. That means that when you declare a function using lexical arrows, "this" inside the function will still be the same "this" as it was outside the function. As a result, we no longer have to pass in the optional "this" argument to forEach!
     Object.keys(rawDataObj).forEach(key => this[key] = rawDataObj[key]);
   }
 
 
   Book.prototype.toHtml = function() {
     var template = Handlebars.compile($('#book-list-template').text());
-    // console.log('in html');
     return template(this);
   };
 
@@ -26,10 +23,8 @@ ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
 
   Book.loadAll = rows => {
     Book.all = rows.sort((a,b) => b.title - a.title).map(book => new Book(book));
-    // console.log(Book.all)
   }
 
-  
   Book.fetchAll = callback => {
     $.get(`${ENV.apiUrl}/api/v1/books`)
       .then(Book.loadAll)
--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -8,14 +8,12 @@ var app = app || {};
   bookView.initIndexPage = function() {
     $('.container').hide();
     $('.book-view').show();
-    // console.log('init index page');
     app.Book.all.map(book => $('.book-stats2').append(book.toHtml()));
   }
   module.bookView = bookView;
 })(app)
 
 $(function() {
-  // console.log('in IIFE');
   app.Book.fetchAll(app.bookView.initIndexPage);
 })
 
